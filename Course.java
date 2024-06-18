ad43a334d8e45c4d7ffe3efca1a9af420953cca8
nomadriding <fundora.a@gmail.com>
2024-06-18 19:11:45 -0400

    create course class

--- Course.java
+++ Course.java
@@ -1,3 +1,28 @@
 public class Course {
+    private String courseId;
+    private String name;
+    private double price;
+    private double money_earned;
+    Teacher teacher;
 
+    public Course(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
